64ccd4aea653a79c87618dff5ada0e647933e9c2
droidecahedron <droidecahedron@gmail.com>
2024-02-01 12:02:56 -0800

    added fxn array for duty cycles instead of individuals, fixed all 4 leds cycling

--- src/main.c
+++ src/main.c
@@ -14,6 +14,8 @@
 
 #define PWM_COUNTERTOP  100
 
+#define NUM_PWM_CHANS 4
+
 static nrfx_pwm_t my_pwm = NRFX_PWM_INSTANCE(1);
 
 static int pwm_init(void)
@@ -24,8 +26,8 @@ static int pwm_init(void)
         {
             28,						// channel 0
             29,						// channel 1
-            31,                 	// channel 2
-            32	                    // channel 3
+            30,                 	// channel 2
+            31	                    // channel 3
         },
         .irq_priority = 5,
         .base_clock   = NRF_PWM_CLK_1MHz,
@@ -41,16 +43,17 @@ static int pwm_init(void)
 	//irq_enable(PWM1_IRQn);
 }
 
-static void pwm_set_duty_cycle(uint32_t dc1, uint32_t dc2)
+static void pwm_set_duty_cycle(uint32_t* duty_cycles)
 {
 	static bool pwm_running = false;
 
     // This array cannot be allocated on stack (hence "static") and it must be in RAM 
     static nrf_pwm_values_individual_t seq_values;
+    uint16_t *pwm_chan_seq = &seq_values; // point to the struct for loop increment instead
 
 	// Update the respective channels
-	seq_values.channel_0 = (dc1 <= PWM_COUNTERTOP) ? dc1 : PWM_COUNTERTOP;
-	seq_values.channel_1 = (dc2 <= PWM_COUNTERTOP) ? dc2 : PWM_COUNTERTOP;
+    for(int pwm_chan=0; pwm_chan < NUM_PWM_CHANS; pwm_chan++)
+	    pwm_chan_seq[pwm_chan] = (duty_cycles[pwm_chan] <= PWM_COUNTERTOP) ? duty_cycles[pwm_chan] : PWM_COUNTERTOP;
 
     nrf_pwm_sequence_t const seq =
     {
@@ -76,10 +79,15 @@ int main(void)
 
 	printk("NRFX PMW example started\n");
 
+    // each ind channel gets its own duty cycle
+    uint32_t duty_cycles[NUM_PWM_CHANS] = {PWM_COUNTERTOP >> 3, PWM_COUNTERTOP >> 2, PWM_COUNTERTOP >> 1, PWM_COUNTERTOP - 1};
+
+    pwm_set_duty_cycle(duty_cycles);
+
 	while (1) {
 		static int counter = 0;
-		pwm_set_duty_cycle(counter, PWM_COUNTERTOP - counter);
-		counter = (counter + 1) % PWM_COUNTERTOP;
+		// pwm_set_duty_cycle(counter, PWM_COUNTERTOP - counter);
+		// counter = (counter + 1) % PWM_COUNTERTOP;
 		k_msleep(SLEEP_TIME_MS);
 	}
 
