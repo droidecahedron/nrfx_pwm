f03bdbeae2f1ec6d71c6f1704cf0b97d8268bc74
droidecahedron <droidecahedron@gmail.com>
2024-02-01 16:22:49 -0800

    got waveform sequence working. same pulse, pauses in between of a time slice

--- src/main.c
+++ src/main.c
@@ -47,31 +47,43 @@ static void pwm_set_duty_cycle(uint32_t* duty_cycles)
 {
 	static bool pwm_running = false;
 
-    // This array cannot be allocated on stack (hence "static") and it must be in RAM 
+    // This array cannot be allocated on stack (hence "static") and it must be in RAM
+    //pass this to the sequence for screenshot A of repo 
     static nrf_pwm_values_wave_form_t seq_values;
     seq_values.channel_0 = PWM_COUNTERTOP/10 | (1 << 15); //flips really early, inverted polarity
     seq_values.channel_1 = PWM_COUNTERTOP/4;
     seq_values.channel_2 = PWM_COUNTERTOP/2; // should flip half way through
     seq_values.counter_top = PWM_COUNTERTOP;
 
-    //TESTING
-    // #define CYCLE_A  (2*1000)   // Some arbitrary number of clock cycles to match required resolution
-    // #define CYCLE_B  (2*2000)	// More clock cycles gets wider
-    // #define CYCLE_C  (2*3000)
-    // #define CYCLE_D  (2*4000)
-    // nrf_pwm_values_wave_form_t peculiarSequence[] = {
-    //     //   Index     Normal pin          Inverted     (Spare)  Top Value
-    //     //   =====     =================== ==========   =======  =========
-    //     { /*   0  */   0x8000|(CYCLE_A/2), (CYCLE_A/2), 0,       CYCLE_A  },
-    //     { /*   1  */   0x8000|(CYCLE_B/2), (CYCLE_B/2), 0,       CYCLE_B  },
-    //     { /*   2  */   0x8000|(CYCLE_C/2), (CYCLE_C/2), 0,       CYCLE_C  },
-    //     { /*   3  */   0x8000|(CYCLE_D/2), (CYCLE_D/2), 0,       CYCLE_D  },
-    // };
+    //screenshot B of repo
+    #define CYCLE_A  (2*1000)   // Some arbitrary number of clock cycles to match required resolution
+    #define CYCLE_B  (2*2000)	// More clock cycles gets wider
+    #define CYCLE_C  (2*3000)
+    #define CYCLE_D  (2*4000)
+    nrf_pwm_values_wave_form_t peculiarSequenceB[] = {
+        //   Index     Normal pin          Inverted     (Spare)  Top Value
+        //   =====     =================== ==========   =======  =========
+        { /*   0  */   0x8000|(CYCLE_A/2), (CYCLE_A/2), 0,       CYCLE_A  },
+        { /*   1  */   0x8000|(CYCLE_B/2), (CYCLE_B/2), 0,       CYCLE_B  },
+        { /*   2  */   0x8000|(CYCLE_C/2), (CYCLE_C/2), 0,       CYCLE_C  },
+        { /*   3  */   0x8000|(CYCLE_D/2), (CYCLE_D/2), 0,       CYCLE_D  },
+    };
+
+    //screenshot C of repo
+    const uint16_t short_pulse = PWM_COUNTERTOP/10 | (1 << 15);
+    static nrf_pwm_values_wave_form_t peculiarSequenceC[] = {
+        { /*   0  */   short_pulse,     PWM_COUNTERTOP, PWM_COUNTERTOP, PWM_COUNTERTOP},
+        { /*   1  */   PWM_COUNTERTOP,  short_pulse,    PWM_COUNTERTOP, PWM_COUNTERTOP},
+        { /*   2  */   PWM_COUNTERTOP,  PWM_COUNTERTOP, short_pulse,    PWM_COUNTERTOP  },
+        { /*   3  */   PWM_COUNTERTOP,  PWM_COUNTERTOP, PWM_COUNTERTOP, PWM_COUNTERTOP  },
+    };
+    //column 1 for channel 1, it's on then off for the rest of the sequence.
+    //column 2 for channel 2, , etc.etc.
 
     nrf_pwm_sequence_t const seq =
     {
-        .values.p_individual = &seq_values,
-        .length              = NRF_PWM_VALUES_LENGTH(seq_values),
+        .values.p_individual = &peculiarSequenceC,
+        .length              = NRF_PWM_VALUES_LENGTH(peculiarSequenceC),
         .repeats             = 0,
         .end_delay           = 0
     };
