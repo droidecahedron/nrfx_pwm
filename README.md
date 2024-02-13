95358f714e6a3409df09307baed0efb1db0235f6
droidecahedron <droidecahedron@gmail.com>
2024-02-13 14:33:17 -0800

    updated to be cycling pwm waveforms

--- README.md
+++ README.md
@@ -7,12 +7,8 @@
 Driving GPIO with PWM peripheral, using nrfx. (https://infocenter.nordicsemi.com/topic/ps_nrf5340/pwm.html?cp=4_0_0_6_22)
 LEDs chosen for visual feedback.
 
-in `pwm_set_duty_cycle`, there are three sequences labelled screenshot A, screenshot B, and screenshot C for some sample outputs.
-when using waveform mode you only get 3 output channels instead of 4.
 
-You just change some of what you pass in that function (all commented) and you can get the below outputs.
 
-> You can address four pwm channels if you don't need to adjust countertop
 
 ![image](https://github.com/droidecahedron/nrfx_pwm/assets/63935881/86076c67-dce1-4ebb-aa1a-80e0461b0765)
 
@@ -25,7 +21,7 @@ GPIO 28-31 for PWM chans
 ## screenshot A
 one instance, 4 outputs, varying duty cycles
 ![image](https://github.com/droidecahedron/nrfx_pwm/assets/63935881/fae9f85b-38fb-4c3f-90cf-1ec1746f04f2)
-You'll need to modify `config0` such that `NRF_PWM_PIN_NOT_CONNECTED` is 31 instead for LED4, the `load_mode` to be `NRF_PWM_LOAD_INDIVIDUAL`, and `p_wave_form` can be `p_individual` where you pass sequence.
+
 
 ## screenshot B
 one instance, 3 outputs, using waveform mode.
@@ -39,3 +35,25 @@ once instance, 3 outputs, same short pulse but delayed by a certain amount. (dep
 
 ### branch: multiple-instance
 multiple instances for distinct frequencies on each i/o. this branch is kind of unfinished because to generate the above screenshots only needed one instance. (thanks @inductivekickback)
+
+# Extra Notes
+
+in `pwm_set_duty_cycle`, there are three sequences for sample outputs that it rotates through.
+when using waveform mode you only get 3 output channels instead of 4. If you need an adjustable countertop, use waveform mode.
+
+You'll need to modify `config0` such that `NRF_PWM_PIN_NOT_CONNECTED` is 31 instead for LED4, the `load_mode` to be `NRF_PWM_LOAD_INDIVIDUAL`, and `p_wave_form` can be `p_individual` where you pass sequence.
+
+You can also have multiple PWM instances.
+
+
+```c
+//if you had a single channel, this is an interesting one.
+// nrf_pwm_values_wave_form_t peculiarSequenceSingleChannel[] = {
+//     //   Index     Normal pin          Inverted     (Spare)  Top Value
+//     //   =====     =================== ==========   =======  =========
+//     { /*   0  */   0x8000|(CYCLE_A/2), (CYCLE_A/2), 0,       CYCLE_A  },
+//     { /*   1  */   0x8000|(CYCLE_B/2), (CYCLE_B/2), 0,       CYCLE_B  },
+//     { /*   2  */   0x8000|(CYCLE_C/2), (CYCLE_C/2), 0,       CYCLE_C  },
+//     { /*   3  */   0x8000|(CYCLE_D/2), (CYCLE_D/2), 0,       CYCLE_D  },
+// }; 
+```
--- src/main.c
+++ src/main.c
@@ -3,125 +3,145 @@
  *
  * Author: Johnny Nguyen (@droidecahedron)
  *
+ * Simple pwm sequence that changes every SLEEP_TIME_MS.
+ *
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <nrfx_pwm.h>
+#include <zephyr/irq.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
-#include <zephyr/irq.h>
-#include <nrfx_pwm.h>
 
-/* 1000 msec = 1 sec */
-#define SLEEP_TIME_MS   50
-
-#define PWM_COUNTERTOP  10000
-
+#define SLEEP_TIME_MS 2000
+#define PWM_COUNTERTOP 10000
 #define NUM_PWM_CHANS 4
+#define NUM_SEQUENCES 3
 
-#define CYCLE_A  (2*1000)   // Some arbitrary number of clock cycles to match required resolution
-#define CYCLE_B  (2*2000)	// More clock cycles gets wider
-#define CYCLE_C  (2*3000)
-#define CYCLE_D  (2*4000)
+// Some arbitrary number of clock cycles to match required resolution, more clock cycles gets wider
+#define CYCLE_A (1 * 1000)
+#define CYCLE_B (2 * 1000)
+#define CYCLE_C (3 * 1000)
+#define CYCLE_D (4 * 1000)
 
 static nrfx_pwm_t my_pwm = NRFX_PWM_INSTANCE(1);
+static uint8_t seq_idx = 0;
 
 static int pwm_init(void)
 {
-	static nrfx_pwm_config_t const config0 =
-    {
+    static nrfx_pwm_config_t const config0 = {
         .output_pins =
-        {
-            28,						// channel 0
-            29,						// channel 1
-            30,                 	// channel 2
-            NRF_PWM_PIN_NOT_CONNECTED	                    // channel 3 but waveform only does 3 at a time. 31 if you want individual for pin4.
-        },
+            {
+                28, // channel 0
+                29, // channel 1
+                30, // channel 2
+                31, // channel 3, NRF_PWM_PIN_NOT_CONNECTED if you want to use waveform to adjust COUNTERTOP
+            },
         .irq_priority = 5,
-        .base_clock   = NRF_PWM_CLK_1MHz,
-        .count_mode   = NRF_PWM_MODE_UP,
-        .top_value    = PWM_COUNTERTOP,
-        .load_mode    = NRF_PWM_LOAD_WAVE_FORM, // change to NRF_PWM_LOAD_INDIVIDUAL for screenshot A
-        .step_mode    = NRF_PWM_STEP_AUTO
-    };
+        .base_clock = NRF_PWM_CLK_1MHz,
+        .count_mode = NRF_PWM_MODE_UP,
+        .top_value = PWM_COUNTERTOP,
+        .load_mode = NRF_PWM_LOAD_INDIVIDUAL, // change to NRF_PWM_LOAD_WAVE_FORM for adjustable COUNTERTOP
+        .step_mode = NRF_PWM_STEP_AUTO};
     return (nrfx_pwm_init(&my_pwm, &config0, NULL, NULL) == NRFX_SUCCESS) ? 0 : -1;
 
-	// If PWM callbacks are to be used, remember to configure the interrupts correctly
-	//IRQ_DIRECT_CONNECT(PWM1_IRQn, 0, nrfx_pwm_1_irq_handler, 0);
-	//irq_enable(PWM1_IRQn);
+    // If PWM callbacks are to be used, remember to configure the interrupts
+    // correctly IRQ_DIRECT_CONNECT(PWM1_IRQn, 0, nrfx_pwm_1_irq_handler, 0);
+    // irq_enable(PWM1_IRQn);
 }
 
-static void pwm_set_duty_cycle(uint32_t* duty_cycles)
+static void pwm_set_duty_cycle(uint8_t sequence_id)
 {
-	static bool pwm_running = false;
+    bool pwm_running = false;
 
-    // This array cannot be allocated on stack (hence "static") and it must be in RAM
-    //pass this to the sequence for screenshot A of repo 
-    static nrf_pwm_values_individual_t seq_values;
-    seq_values.channel_0 = PWM_COUNTERTOP/10 | (1 << 15); //flips really early, inverted polarity
-    seq_values.channel_1 = PWM_COUNTERTOP/4;
-    seq_values.channel_2 = PWM_COUNTERTOP/2; // should flip half way through
-    seq_values.channel_3 = PWM_COUNTERTOP/10;
+    // This array cannot be allocated on stack (hence "static") and it must be in
+    // RAM note: if using waveform mode for adjustable countertop, the sequence
+    // data type is nrf_pwm_values_wave_form_t
+    static nrf_pwm_values_individual_t sequence_A;          // sequence A is a pretty basic PWM duty cycle.
+    sequence_A.channel_0 = PWM_COUNTERTOP / 10 | (1 << 15); // flips really early, inverted polarity
+    sequence_A.channel_1 = PWM_COUNTERTOP / 4;
+    sequence_A.channel_2 = PWM_COUNTERTOP / 2; // should flip half way through
+    sequence_A.channel_3 = PWM_COUNTERTOP / 10;
 
-    //if you had a single channel, this is an interesting one.
-    // nrf_pwm_values_wave_form_t peculiarSequenceSingleChannel[] = {
-    //     //   Index     Normal pin          Inverted     (Spare)  Top Value
-    //     //   =====     =================== ==========   =======  =========
-    //     { /*   0  */   0x8000|(CYCLE_A/2), (CYCLE_A/2), 0,       CYCLE_A  },
-    //     { /*   1  */   0x8000|(CYCLE_B/2), (CYCLE_B/2), 0,       CYCLE_B  },
-    //     { /*   2  */   0x8000|(CYCLE_C/2), (CYCLE_C/2), 0,       CYCLE_C  },
-    //     { /*   3  */   0x8000|(CYCLE_D/2), (CYCLE_D/2), 0,       CYCLE_D  },
-    // }; 
-    //screenshot B of repo
-    nrf_pwm_values_wave_form_t peculiarSequenceB[] = {
-        {0x8000|(CYCLE_A/2), 0x8000|(CYCLE_B/2),    0x8000|(CYCLE_C/2), CYCLE_A},
-        {(CYCLE_A/2),        (CYCLE_B/2),           (CYCLE_C/2),        CYCLE_B},
-        {0,                  0,                     0,                  CYCLE_C},
-        {CYCLE_A,            CYCLE_B,               CYCLE_C,            CYCLE_D},
+    // column 1 for channel 1, it's on then off for the rest of the sequence.
+    // column 2 for channel 2, , etc.
+
+    // a little more interesting, dilating cycles
+    static nrf_pwm_values_individual_t sequence_B[] = {
+        {0x8000 | (CYCLE_A / 2), 0x8000 | (CYCLE_B / 2), 0x8000 | (CYCLE_C / 2), CYCLE_A},
+        {(CYCLE_A / 2), (CYCLE_B / 2), (CYCLE_C / 2), CYCLE_B},
+        {0, 0, 0, CYCLE_C},
+        {CYCLE_A, CYCLE_B, CYCLE_C, CYCLE_D},
     };
 
-    //screenshot C of repo
-    const uint16_t short_pulse = PWM_COUNTERTOP/10 | (1 << 15);
-    static nrf_pwm_values_wave_form_t peculiarSequenceC[] = {
-        { /*   0  */   short_pulse,     PWM_COUNTERTOP, PWM_COUNTERTOP, PWM_COUNTERTOP},
-        { /*   1  */   PWM_COUNTERTOP,  short_pulse,    PWM_COUNTERTOP, PWM_COUNTERTOP},
-        { /*   2  */   PWM_COUNTERTOP,  PWM_COUNTERTOP, short_pulse,    PWM_COUNTERTOP  },
-        { /*   3  */   PWM_COUNTERTOP,  PWM_COUNTERTOP, PWM_COUNTERTOP, PWM_COUNTERTOP  },
+    // short pulses, then OFF.
+    const uint16_t short_pulse = PWM_COUNTERTOP / 10 | (1 << 15);
+    static nrf_pwm_values_individual_t sequence_C[] = {
+        {/*   0  */ short_pulse, PWM_COUNTERTOP, PWM_COUNTERTOP, PWM_COUNTERTOP},
+        {/*   1  */ PWM_COUNTERTOP, short_pulse, PWM_COUNTERTOP, PWM_COUNTERTOP},
+        {/*   2  */ PWM_COUNTERTOP, PWM_COUNTERTOP, short_pulse, PWM_COUNTERTOP},
+        {/*   3  */ PWM_COUNTERTOP, PWM_COUNTERTOP, PWM_COUNTERTOP, short_pulse},
     };
-    //column 1 for channel 1, it's on then off for the rest of the sequence.
-    //column 2 for channel 2, , etc.etc.
 
-    nrf_pwm_sequence_t const seq =
+    const uint16_t long_pulse = PWM_COUNTERTOP / 2 | (1 << 15);
+    static nrf_pwm_values_individual_t sequence_D[] = {
+        {/*   0  */ short_pulse, PWM_COUNTERTOP, long_pulse, PWM_COUNTERTOP},
+        {/*   1  */ PWM_COUNTERTOP, long_pulse, PWM_COUNTERTOP, short_pulse},
+        {/*   2  */ long_pulse, PWM_COUNTERTOP, short_pulse, PWM_COUNTERTOP},
+        {/*   3  */ PWM_COUNTERTOP, short_pulse, PWM_COUNTERTOP, long_pulse},
+    };
+
+    // sequence args for simple playback
+    // use member element .p_wave_form for waveform mode.
+    nrf_pwm_sequence_t const seqA = {
+        .values.p_individual = &sequence_A, .length = NRF_PWM_VALUES_LENGTH(sequence_A), .repeats = 0, .end_delay = 0};
+    nrf_pwm_sequence_t const seqB = {
+        .values.p_individual = &sequence_B, .length = NRF_PWM_VALUES_LENGTH(sequence_B), .repeats = 0, .end_delay = 0};
+    nrf_pwm_sequence_t const seqC = {
+        .values.p_individual = &sequence_C, .length = NRF_PWM_VALUES_LENGTH(sequence_C), .repeats = 0, .end_delay = 0};
+    nrf_pwm_sequence_t const seqD = {
+        .values.p_individual = &sequence_D, .length = NRF_PWM_VALUES_LENGTH(sequence_D), .repeats = 0, .end_delay = 0};
+
+    if (!pwm_running)
     {
-        .values.p_wave_form = &peculiarSequenceC, // use member element .p_individual for screenshot A
-        .length              = NRF_PWM_VALUES_LENGTH(peculiarSequenceB),
-        .repeats             = 0,
-        .end_delay           = 0
-    };
-
-	if(!pwm_running){
-		pwm_running = true;
-		(void)nrfx_pwm_simple_playback(&my_pwm, &seq, 1000, NRFX_PWM_FLAG_LOOP);
-	}
+        printk("Sequence num: %d\n", sequence_id);
+        pwm_running = true;
+        switch (sequence_id)
+        {
+        case 0:
+            (void)nrfx_pwm_simple_playback(&my_pwm, &seqA, 1000, NRFX_PWM_FLAG_LOOP);
+            break;
+        case 1:
+            (void)nrfx_pwm_simple_playback(&my_pwm, &seqB, 1000, NRFX_PWM_FLAG_LOOP);
+            break;
+        case 2:
+            (void)nrfx_pwm_simple_playback(&my_pwm, &seqC, 1000, NRFX_PWM_FLAG_LOOP);
+            break;
+        case 3:
+            (void)nrfx_pwm_simple_playback(&my_pwm, &seqD, 1000, NRFX_PWM_FLAG_LOOP);
+            break;
+        default:
+            break;
+        }
+    }
 }
-
 
 int main(void)
 {
-	if(pwm_init() != 0){
-		printk("Error initializing PWM\n");
-		return -1;
-	}
+    if (pwm_init() != 0)
+    {
+        printk("Error initializing PWM\n");
+        return -1;
+    }
 
-	printk("NRFX PMW example started\n");
+    printk("NRFX PMW example started\n");
 
-    // each ind channel gets its own duty cycle
-    uint32_t duty_cycles[NUM_PWM_CHANS] = {PWM_COUNTERTOP >> 3, PWM_COUNTERTOP >> 2, PWM_COUNTERTOP >> 1, PWM_COUNTERTOP - 1};
-
-    pwm_set_duty_cycle(duty_cycles);
-
-	while (1) {
-		k_msleep(SLEEP_TIME_MS);
-	}
+    while (1)
+    {
+        pwm_set_duty_cycle((seq_idx++ & NUM_SEQUENCES)); // mask the overflow for circular buf.
+                                                         // only works for powers of 2 - 1.
+        k_msleep(SLEEP_TIME_MS);
+    }
 
     return 0;
 }
